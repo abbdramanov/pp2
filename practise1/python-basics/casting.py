d2280080429768ff37db7e584b4e866d84e90d63
Maratuly Akhmet <ak_maratuly@kbtu.kz>
2026-02-03 13:32:30 +0500

    Add Practice1 Python basics examples

---
+++ practise1/python-basics/casting.py
@@ -0,0 +1,30 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+
+
+x = complex(1)     # x will be (1+0j)
+y = complex(2.8)   # y will be (2.8+0j)
+z = complex("3")   # z will be (3+0j)
+w = complex("4.2") # w will be (4.2+0j)
+
+
+
+x = complex(5)  # x will be (5+0j)
+y = complex(6)  # y will be (6+0j)
+z = complex(7)  # z will be (7+0j)
+w = complex(8)  # w will be (8+0j)
---
+++ practise1/python-basics/syntax.py
@@ -0,0 +1,15 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+        
+
+if 6 > 7:
+    print("Six is greater than seven!")
+
+
